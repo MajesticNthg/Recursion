721cd0bb0219c2cf048a1da16dc843d3cc3a5eed
Danil <147924429+MajesticNthg@users.noreply.github.com>
2024-06-18 17:21:43 +1000

    Correct size list (without delete first element)

--- src/Delete.java
+++ src/Delete.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 public class Delete {
     public static int delete (ArrayList<Integer> myList) {
-        if (myList.size() <= 1) return 0;
+        if (myList.size() == 0) return 0;
         myList.remove(0);
 
         return 1 + delete(myList);
