8ad13e2194d21037d457c0e9920e2771f397b96d
Danil <147924429+MajesticNthg@users.noreply.github.com>
2024-06-26 21:19:13 +1000

    UPD method 2 without int type

--- src/TwoMax.java
+++ src/TwoMax.java
@@ -1,25 +1,25 @@
 import java.util.ArrayList;
 
 public class TwoMax {
-    public static int enterMax (ArrayList <Integer> myList) {
-        int max1 = 0;
-        int max2 = 0;
-        int amt = 0;
-        max2 = secondMax(myList, max1, max2, 0);
-        return max2;
+    public static int enterMax(ArrayList<Integer> myList) {
+        secondMax(myList);
+
+        return myList.get(1);
     }
-    public static int secondMax (ArrayList<Integer> myList, int max1, int max2, int amt) {
-        if (amt >= myList.size()) return max2;
-        if (myList.get(amt) >= max1) {
-            max2 = max1;
-            max1 = myList.get(amt);
-        }
-        if (myList.get(amt) >= max2 && myList.get(amt) < max1) {
-            max2 = myList.get(amt);
+
+    private static void secondMax(ArrayList<Integer> myList) {
+        boolean sort = true;
+
+        for (int i = 0; i < myList.size() - 1; i++) {
+            if (myList.get(i + 1) > myList.get(i)) {
+                myList.add(i, myList.get(i + 1));
+                myList.remove(i + 2);
+                sort = false;
+            }
+            if (i == myList.size() - 2 && sort == true) return;
         }
 
-       return secondMax(myList, max1, max2, amt + 1);
-
+        secondMax(myList);
     }
 }
 
