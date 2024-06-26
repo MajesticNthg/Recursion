f3e1edefb2527aa90a1e9d7b25e9d97d9bb7b896
Danil <147924429+MajesticNthg@users.noreply.github.com>
2024-06-26 18:37:30 +1000

    Exchange method 1 and method 2, add parameter in method 1

--- src/SearchFiles.java
+++ src/SearchFiles.java
@@ -3,30 +3,30 @@ import java.io.File;
 import java.util.Arrays;
 
 public class SearchFiles {
-    public static ArrayList createList() {
-        ArrayList<File> myList = new ArrayList<File>(); // пустой список, в котором будут файлы
-        File root = new File("."); // директория каталога
-        ArrayList<File> expand = new ArrayList<File>(); // список каталога
-        expand.add(root); // заполняю список каталога
+    public static ArrayList SearchFiles(String path) {
+        ArrayList<File> myList = new ArrayList<File>();
+        File root = new File(path);
+        ArrayList<File> expand = new ArrayList<File>();
+        expand.add(root);
 
-        return SearchFiles(myList, expand);
+        return createList(myList, expand);
     }
 
-    public static ArrayList<File> SearchFiles(ArrayList<File> myList, ArrayList<File> expand) {
-        if (expand.isEmpty()) return myList; // файлов нет, каталогов нет
+    private static ArrayList<File> createList(ArrayList<File> myList, ArrayList<File> expand) {
+        if (expand.isEmpty()) return myList;
 
-        File[] expandCopy = expand.toArray(new File[expand.size()]); // копия списка каталога
-        expand.clear(); // обнуляю список каталога
+        File[] expandCopy = expand.toArray(new File[expand.size()]);
+        expand.clear();
 
-        for (File file : expandCopy) { // цикл по каталогу
-            if (file.isDirectory()) { // если файл директория, то погружаюсь в директорию
-                expand.addAll(Arrays.asList(file.listFiles())); // перезаписываю список каталога подкаталогом
+        for (File file : expandCopy) {
+            if (file.isDirectory()) {
+                expand.addAll(Arrays.asList(file.listFiles()));
             } else {
-                myList.add(file); // добавляю файл в список
+                myList.add(file);
             }
         }
 
-        return SearchFiles(myList, expand); // возвращаю заполненный список с файлами
+        return createList(myList, expand);
     }
 }
 
